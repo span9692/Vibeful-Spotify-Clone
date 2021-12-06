79fa3f01ec2264508c4f7f1a16ff3fe2d42ca570
Anthony Fahden <afahden@gmail.com>
2021-12-06 13:59:41 -0800

    starting on DELETE route for playlists

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -10,7 +10,4 @@ def all_playlists():
     playlists = Playlist.query.all()
     return {'playlists': [playlist.to_dict() for playlist in playlists]}
 
-    # users = User.query.all()
-    # return {'users': [user.to_dict() for user in users]}
 
-    # return playlists
