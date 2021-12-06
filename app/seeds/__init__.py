7a44d03ecb9e963173e14eb1f747490d2b4613af
Sean Pan <seanhpan@gmail.com>
2021-12-05 19:44:13 -0800

    playlist_song seed in progress

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .songs import seed_songs, undo_songs
 from .playlists import seed_playlists, undo_playlists
+from .playlist_songs import seed_playlist_songs, undo_playlist_songs
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,6 +15,7 @@ def seed():
     seed_users()
     seed_songs()
     seed_playlists()
+    seed_playlist_songs()
     # Add other seed functions here
 
 
@@ -23,4 +25,5 @@ def undo():
     undo_users()
     undo_songs()
     undo_playlists()
+    undo_playlist_songs()
     # Add other undo functions here
