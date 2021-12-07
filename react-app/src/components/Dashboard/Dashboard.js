70ee44b324542090f38da4f94903b05b7904dbf8
davidvous <ledavidvu@gmail.com>
2021-12-07 14:31:51 -0800

    styled player

--- react-app/src/components/Dashboard/Dashboard.js
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import Playlist from "../Playlists/Playlist"
+import Player from '../Player/index'
 import "./Dashboard.css";
 
 const Dashboard = () => {
+
+  const user = useSelector((state) => state.session.user);
 
   return (
     <>
@@ -10,92 +15,42 @@ const Dashboard = () => {
         <div className="dashboard-main">
           <div className="dashboard-main-leftnav">
             <div className="d-m-leftnav-top">
+              
               <ul>
                 <li>
-                  <i className="fas fa-search"></i> Search
+                  <div className="d-m-leftnav-item">
+                  <i className="fas fa-search"></i><span>Search</span>
+                  </div>
                 </li>
                 <li>
-                  <i className="fas fa-icons"></i>   Your library
+                  <div className="d-m-leftnav-item">
+                  <i className="fas fa-icons"></i><span>Your library</span>
+                  </div>
+                </li>
+              </ul>
+              <div className="liner"></div>
+              <ul>
+                <li>
+                  <div className="d-m-leftnav-item">
+                  <i class="fas fa-microphone-alt"></i><span>Add a song</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="d-m-leftnav-item">
+                  <i class="fab fa-napster"></i><span>Create a playlist</span>
+                  </div>
                 </li>
               </ul>
             </div>
-                <div className="liner"></div>
-            <div className="d-m-leftnav-bot">
-              <ul>
-                <li>Playlist 1 1232312 2132 13213 </li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-                <li>Playlist 1</li>
-                <li>Playlist 2</li>
-              </ul>
-            </div>
+
+            <div className="liner"></div>
+            <Playlist />
           </div>
-          <div className="dashboard-main-rightcontent">Dashboard</div>
+          <div className="dashboard-main-rightcontent">
+            Welcome {user.first_name}!
+          </div>
         </div>
-        <div className="dashboard-bot-player">Player</div>
+        <div className="dashboard-bot-player"><Player /></div>
       </div>
     </>
   );
