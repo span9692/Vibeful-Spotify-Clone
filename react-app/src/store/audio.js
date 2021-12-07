f8f5094dcaa8220fd1a563c9b7cde3ae2710daa5
Sean <seanhpan@gmail.com>
2021-12-07 01:27:36 -0800

    audio player working

---
+++ react-app/src/store/audio.js
@@ -0,0 +1,21 @@
+const PLAY_SONG = 'songs/PLAY_SONGS'
+
+export const playMusic = (data) => {
+    return {
+        type: PLAY_SONG,
+        data
+    }
+}
+
+export default function reducer(state = {}, action) {
+    let newState;
+    switch (action.type) {
+        case PLAY_SONG:
+            newState = {...state}
+            newState = action.data
+            console.log('newstate', newState)
+            return newState;
+        default:
+            return state;
+    }
+}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import song from './song'
+import audio from './audio'
 
 const rootReducer = combineReducers({
   session,
   song,
+  audio
 });
 
 
