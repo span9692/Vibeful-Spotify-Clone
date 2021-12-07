3e254bcb9958f6eb98e9ab6af401d5ddf5da15d1
davidvous <ledavidvu@gmail.com>
2021-12-07 08:26:49 -0800

    final push

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
       <nav>
         <div class="header"></div>
         <NavLink to="/home" exact={true} activeClassName="active">
-        <img className="logo" src="https://cdn.discordapp.com/attachments/917541871457275925/917759128548569098/vibe_icon.png" />
+        <img alt="logo" className="logo" src="https://cdn.discordapp.com/attachments/917541871457275925/917759128548569098/vibe_icon.png" />
         </NavLink>
         <NavLink to="/home" exact={true} activeClassName="active" className="homeLink">
           Home
