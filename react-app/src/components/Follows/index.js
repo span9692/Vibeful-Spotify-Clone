e06fa954218b2f6849d6ba8eeb2a36c926ad5e4f
Travis <travisbly@gmail.com>
2021-12-07 22:54:49 -0800

    completed follows front end route

--- react-app/src/components/Follows/index.js
+++ react-app/src/components/Follows/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router";
 
@@ -6,12 +6,15 @@ import { showFollowing, unfollowUser } from "../../store/follow";
 
 const Follows = () => {
   const dispatch = useDispatch();
+  const [load, setLoad] = useState(false);
   const { id } = useParams();
-  const follows = useSelector((state) => Object.values(state.follow.myFollows));
+  const follows = useSelector((state) =>
+    Object.values(state.follow.myFollows)
+  )[0];
   console.log("THISBEDAFOLLOWS-------------->", follows);
 
   useEffect(() => {
-    dispatch(showFollowing(id));
+    dispatch(showFollowing(id)).then(() => setLoad(true));
   }, [dispatch, id]);
 
   const handleDelete = (id) => {
@@ -19,15 +22,19 @@ const Follows = () => {
   };
 
   return (
-    <div>
-      <h1>FOLLOWS</h1>
-      {follows.map((follow) => (
-        <div key={follow.id}>
-          {follow.first_name} -- {follow.last_name}
-          <button onClick={() => handleDelete(follow.id)}>Unfollow user</button>
-        </div>
-      ))}
-    </div>
+    load && (
+      <div>
+        <h1>FOLLOWS</h1>
+        {follows.map((follow) => (
+          <div key={follow.id}>
+            {follow.first_name} -- {follow.last_name}
+            <button onClick={() => handleDelete(follow.id)}>
+              Unfollow user
+            </button>
+          </div>
+        ))}
+      </div>
+    )
   );
 };
 
