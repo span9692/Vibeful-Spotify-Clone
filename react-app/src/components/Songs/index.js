f0964f9f13bf379302d4fa7f63d81dc58bbb6b34
Sean <seanhpan@gmail.com>
2021-12-07 22:19:41 -0800

    conditional rendering

--- react-app/src/components/Songs/index.js
+++ react-app/src/components/Songs/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getSongs } from '../../store/song'
 import { playMusic } from '../../store/audio'
@@ -8,27 +8,30 @@ import { getPlaylists } from '../../store/playlist'
 
 function SongList() {
     const dispatch = useDispatch()
+    const [isLoaded, setIsLoaded] = useState(false)
 
     const songs = useSelector(state => Object.values(state.song))
     const userId = useSelector(state => state.session.user.id)
     const playlists = useSelector(state => Object.values(state.playlist))
     const currentUserLibrary = playlists.filter(el => el.owner_id == userId && el.playlist_name == 'Library')[0]
+    const playlist_songs = useSelector(state => state.playlist_song)
 
     console.log('playlistsssssssssssss', playlists)
     console.log('currentUserLibraryyyyyyyyyyyyyyy', currentUserLibrary)
+    console.log('songggggggggggggggggggggggggggg', songs)
+    console.log('playlist songgggggggggggsssssssssss', playlist_songs)
 
 
 
     useEffect(() => {
         dispatch(getSongs())
         dispatch(getPlaylists())
-        dispatch(getLibrary())
+        dispatch(getLibrary()).then(()=>setIsLoaded(true))
     }, [dispatch])
 
     const play = (song) => {
         dispatch(playMusic(song))
     }
-
     
     const addLibrarySong = (song) => {
         dispatch(addToLibrary(song, currentUserLibrary))
@@ -39,16 +42,23 @@ function SongList() {
     }
 
     
-    return (
+    return isLoaded && (
         <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             HELLO FROM SONGLIST
             {songs.map(song => (
-                <div key={song.id}>
+                <div key={song?.id}>
                     <button onClick={() => play(song)}>{song.title} -- {song.artist}</button>
-                    {/* {checkLibrarySongExists(song) ? "100" : "3"} */}
-                    {/* {false ? "100" : "3"} */}
-                    <button onClick={() => addLibrarySong(song)}>Like</button>
-                    <button onClick={() => removeLibrarySong(song)}>Unlike</button>
+                    {song?.id}
+                    and
+                    {playlist_songs[currentUserLibrary.id].includes(song.id) ?
+                    <button onClick={() => removeLibrarySong(song)}>Unlike</button> :
+                    <button onClick={() => addLibrarySong(song)}>Like</button> 
+                    }
                     <button>Add to Playlist</button>
                 </div>
 
