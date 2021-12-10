9cbc7b2269a1f43885102243e114339733ab84f5
Sean Pan <seanhpan@gmail.com>
2021-12-10 14:20:10 -0800

    dashboard in progress

--- react-app/src/components/RowSong/RowSong.js
+++ react-app/src/components/RowSong/RowSong.js
@@ -1,15 +1,52 @@
+import IndivSong from "../Songs/IndivSong";
 import "./RowSong.css";
 
-function RowSong(urlId, allSongs, allPlaylists, allPlaylistSongs) {
+function RowSong({urlId, allSongs, currentUserLibraryId, allPlaylists, allPlaylistSongs}) {
+  console.log('urlId', urlId)
+  console.log('allSongs',allSongs)
+  console.log('allPlaylists',allPlaylists)
+  console.log('allPlaylistSongs',allPlaylistSongs) //joins table
+
+  let currentUserLibrary = allPlaylists.filter(el => el.owner_id == urlId && el.playlist_name == 'Library')[0]
+  // let currentUserLibraryId = currentUserLibrary?.id //just the library 'playlist' id
+
+  let songId = allPlaylistSongs[currentUserLibraryId.toString()]
+
+  if (!songId) { //if library is empty
+    songId = []
+  }
+
+  const shuffle = (arr) => {
+    arr.sort(()=>Math.random()-0.5);
+    return arr
+  }
+
+  let relevantSongs = allSongs.filter(el => songId.includes(el.id)) //randomize songs
+  shuffle(relevantSongs)
+
+  if (relevantSongs.length > 5) { //only display 5 songs
+    relevantSongs = relevantSongs.slice(0,5)
+  }
+  console.log('relevantSongs', relevantSongs)
+
   return (
     <>
-      <h2>Recent Songs</h2>
-      <div className="main_row">
-        <div>Song 1</div>
-        <div>Song 2</div>
-        <div>Song 3</div>
-        <div>Song 4</div>
-        <div>Song 5</div>
+      <h2>Liked Songs</h2>
+      <div>
+        <table>
+          <tr className='tableHeader'>
+              <th>#</th>
+              <th>Title</th>
+              <th></th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th></th>
+              <th></th>
+          </tr>
+          {relevantSongs.map((song, index) => (
+        <IndivSong key={song.id} index={index} song={song} currentUserLibrary={currentUserLibrary} playlist_songs={allPlaylistSongs} playlists={allPlaylists}/>
+      ))}
+        </table>
       </div>
       <br></br>
       <br></br>
@@ -18,11 +55,9 @@ function RowSong(urlId, allSongs, allPlaylists, allPlaylistSongs) {
       <br></br>
       <h2>Recent Playlists</h2>
       <div className="main_row">
-        <div>Playlist 1</div>
-        <div>Playlist 2</div>
-        <div>Playlist 3</div>
-        <div>Playlist 4</div>
-        <div>Playlist 5</div>
+        {relevantSongs.map(song => (
+          <img className='coverImg' src={song.cover}/>
+        ))}
       </div>
       <br></br>
       <br></br>
