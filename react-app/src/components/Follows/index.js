10a4adb8e3203366f39a4468ab0279d594c5f89c
Sean Pan <seanhpan@gmail.com>
2021-12-11 15:40:35 -0800

    social in progress

--- react-app/src/components/Follows/index.js
+++ react-app/src/components/Follows/index.js
@@ -2,27 +2,31 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
+import { getAllUsers } from "../../store/alluser";
 
 import { showFollowing, unfollowUser, followUser } from "../../store/follow";
 
 const Follows = ({ follower_id, followee_id }) => {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
+  // const [load, setLoad] = useState(false);
 
   const dispatch = useDispatch();
-  const [load, setLoad] = useState(false);
   const { id } = useParams();
-  const followers = useSelector((state) => Object.values(state.follow))[0];
   const userId = useSelector((state) => state.session.user.id);
+
+  const everyone = useSelector(state => state.alluser)
+  const followers = useSelector((state) => Object.values(state.follow))[0];
   const follows = useSelector((state) => Object.values(state.follow))[1];
 
+  console.log('everyone', everyone)
   // console.log("THIS IS THE follower_ID", follower_id);
   // console.log("THIS IS THE FOLLOWEE_ID", followee_id);
   // console.log("THISBEDA FOLLOWERS-------------->", followers);
   // console.log("THISBEDA FOLLOWS-------------->", follows);
 
   useEffect(() => {
-    dispatch(showFollowing(id)).then(() => setLoad(true));
-  }, [dispatch, id]);
+    dispatch(getAllUsers())
+  }, [dispatch]);
 
   const handleDelete = (follower_id) => {
     dispatch(unfollowUser(follower_id, followee_id));
@@ -31,55 +35,55 @@ const Follows = ({ follower_id, followee_id }) => {
     dispatch(followUser(id));
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("/api/users/");
-      const responseData = await response.json();
-      setUsers(responseData.users);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch("/api/users/");
+  //     const responseData = await response.json();
+  //     setUsers(responseData.users);
+  //   }
+  //   fetchData();
+  // }, []);
 
-  const userComponents = users.map((user) => {
-    return (
-      <li key={user.id}>
-        <ul>
-          {user.first_name} {user.last_name}{" "}
-          <button onClick={() => handleDelete(user.id)}>Unfollow user</button>
-          <button onClick={() => handleAddFollower(user.id)}>
-            Follow user
-          </button>
-        </ul>
-      </li>
-    );
-  });
+  // const userComponents = users.map((user) => {
+  //   return (
+  //     <li key={user.id}>
+  //       <ul>
+  //         {user.first_name} {user.last_name}{" "}
+  //         <button onClick={() => handleDelete(user.id)}>Unfollow user</button>
+  //         <button onClick={() => handleAddFollower(user.id)}>
+  //           Follow user
+  //         </button>
+  //       </ul>
+  //     </li>
+  //   );
+  // });
 
   return (
-    load && (
-      <div>
+      <div className='tablediv'>
+        <div className='pageTitle'><div>Social Circle</div></div>
+        <div className='subTitle'>Stay connected through music!</div>
         <div>
           <div>
             <h1>User List: </h1>
-            <ul>{userComponents}</ul>
+            {/* <ul>{userComponents}</ul> */}
           </div>
           <h1>FOLLOWING</h1>
-          {follows.map((follow) => (
+          {/* {follows.map((follow) => (
             <div key={follow.id}>
               {follow.first_name} {follow.last_name}
             </div>
-          ))}
+          ))} */}
         </div>
         <div>
           <h1>FOLLOWERS</h1>
-          {followers.map((follower) => (
+          {/* {followers.map((follower) => (
             <div key={follower.id}>
               {follower.first_name} {follower.last_name}
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     )
-  );
 };
 
 export default Follows;
---
+++ react-app/src/store/alluser.js
@@ -0,0 +1,27 @@
+const GET_ALL_USERS = 'follow/GET_ALL_USERS'
+
+const everyUser = (data) => {
+    return {
+        type: GET_ALL_USERS,
+        data
+    }
+}
+
+export const getAllUsers = () => async (dispatch) => {
+    const response = await fetch('/api/follow/allUsers')
+    const data = await response.json()
+    dispatch(everyUser(data))
+}
+
+export default function reducer(state = {}, action) {
+    let newState;
+    switch (action.type) {
+        case GET_ALL_USERS:
+            newState = {};
+            console.log('action.data', action.data)
+            action.data.users.forEach(user => newState[user.id] = user)
+            return newState
+        default:
+            return state;
+    }
+}
