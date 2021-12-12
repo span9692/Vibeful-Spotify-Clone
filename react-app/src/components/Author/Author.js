7ff3a67afcb2fced32b81596d9602db25b2a7ebf
Sean Pan <seanhpan@gmail.com>
2021-12-12 10:20:25 -0800

    add follow route functional

--- react-app/src/components/Author/Author.js
+++ react-app/src/components/Author/Author.js
@@ -6,21 +6,21 @@ const Author = () => {
         {
         name: "David Le",
         github: "https://github.com/davidvous",
-        linkedin: "/",
+        linkedin: "https://www.linkedin.com/in/david-vu-le/",
       }, {
         name: "Sean Pan",
         github: "https://github.com/span9692",
-        linkedin: "/",
-      }, 
+        linkedin: "https://www.linkedin.com/in/sean-pan-395a4593/",
+      },
       {
         name: "Anthony Fahden",
         github: "https://github.com/juniporous",
-        linkedin: "/",
+        linkedin: "https://www.linkedin.com/in/anthony-fahden-a9251260/",
       },
       {
         name: "Travis Ly",
         github: "https://github.com/lytravis",
-        linkedin: "/",
+        linkedin: "https://www.linkedin.com/in/lytravis/",
       }
     ];
 
@@ -28,21 +28,27 @@ const Author = () => {
     <>
       {authors.map((each) => (
         <div className="author-box-info">
-          <h3>{each.name}</h3>
-          <a href={`${each.github}`}>
-            <img
-              className="github_icon"
-              alt="github_icon"
-              src="https://cdn.discordapp.com/attachments/907133739128217621/907463508101316659/github.png"
-            />
-          </a>
-          <a href={`${each.linkedin}`}>
-            <img
-              className="linkedin_icon"
-              alt="linkedin_icon"
-              src="https://media.discordapp.net/attachments/907133739128217621/907465323828084746/linkedin.png"
-            />
-          </a>
+          <div className='namebox'>
+            <h3>{each.name}</h3>
+          </div>
+          <div className='gitbox'>
+            <a href={`${each.github}`}>
+              <img
+                className="github_icon"
+                alt="github_icon"
+                src="https://cdn.discordapp.com/attachments/907133739128217621/907463508101316659/github.png"
+              />
+            </a>
+          </div>
+          <div className='linkedbox'>
+            <a href={`${each.linkedin}`}>
+              <img
+                className="linkedin_icon"
+                alt="linkedin_icon"
+                src="https://media.discordapp.net/attachments/907133739128217621/907465323828084746/linkedin.png"
+              />
+            </a>
+          </div>
         </div>
       ))}
     </>
--- react-app/src/store/follow.js
+++ react-app/src/store/follow.js
@@ -72,17 +72,13 @@ export default function reducer(state = {}, action) {
       newState = action.data
       return newState;
     case ADD_FOLLOW:
-      console.log('state', state)
-      console.log('in action.data', action.data)
-      console.log('first newState', newState)
       newState = {...state};
-      console.log('in newState before update', newState)
+
       if (!(action.data["follower_id"]['id'] in newState)) {
         newState[action.data["follower_id"]['id']] = [];
       }
       newState[action.data["follower_id"]['id']]['followees'].push(action.data["followee_id"]);
       newState[action.data["followee_id"]['id']]['followers'].push(action.data["follower_id"]);
-      console.log('in newState after update', newState)
       return newState;
     case REMOVE_FOLLOW:
       newState = { ...state };
