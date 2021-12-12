e8c213b41bded930795e6316c05861a988958a3d
Sean Pan <seanhpan@gmail.com>
2021-12-11 17:48:47 -0800

    in progress

--- react-app/src/components/Follows/index.js
+++ react-app/src/components/Follows/index.js
@@ -7,9 +7,6 @@ import { getAllUsers } from "../../store/alluser";
 import { showFollowing, unfollowUser, followUser } from "../../store/follow";
 
 const Follows = ({ follower_id, followee_id }) => {
-  // const [users, setUsers] = useState([]);
-  // const [load, setLoad] = useState(false);
-
   const dispatch = useDispatch();
   const { id } = useParams();
   const userId = useSelector((state) => state.session.user.id);
@@ -18,15 +15,11 @@ const Follows = ({ follower_id, followee_id }) => {
   const followers = useSelector((state) => Object.values(state.follow))[0];
   const followees = useSelector((state) => Object.values(state.follow))[1];
 
-  console.log('everyone', everyone)
-  // console.log("THIS IS THE follower_ID", follower_id);
-  // console.log("THIS IS THE FOLLOWEE_ID", followee_id);
-  // console.log("THISBEDA FOLLOWERS-------------->", followers);
-  // console.log("THISBEDA FOLLOWS-------------->", follows);
+  const [users, setUsers] = useState(everyone);
 
   useEffect(() => {
     dispatch(getAllUsers())
-  }, [dispatch]);
+  }, [dispatch, users]);
 
   const handleDelete = (follower_id) => {
     dispatch(unfollowUser(follower_id, followee_id));
@@ -35,16 +28,28 @@ const Follows = ({ follower_id, followee_id }) => {
     dispatch(followUser(id));
   };
 
+  const showEveryone = () => {
+    setUsers(everyone)
+  }
+
+  const showFollowers = () => {
+    setUsers(followers)
+  }
+
+  const showFollowees = () => {
+    setUsers(followees)
+  }
   // everyone is a list of ALL the users
-  // followee is a list of users that YOU follow
+  // followees is a list of users that YOU follow
   // followers is a list of users that follow you
 
   return (
       <div className='tablediv'>
         <div className='pageTitle'><div>Social Circle</div></div>
         <div className='subTitle'>Stay connected through music!</div>
+        <button onClick={()=>showEveryone()}>everyone</button><button onClick={()=>showFollowers()}>your followers</button><button onClick={()=>showFollowees()}>people YOU follow</button>
         <div className="main_row">
-          {everyone.map(individual => (
+          {users.map(individual => (
             <div key={individual.id}>
               <Link className="sub_row" to={`/users/${individual.id}/dashboard`}>
                 <img className='subsubrow social_image' src={individual.profile_pic} alt='Image Not Found'></img>
